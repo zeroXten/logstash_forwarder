2910259f91769c31e70545daee2698d6ae7448ce
Fraser Scott <fraser.scott@burberry.com>
2014-02-18 11:04:30 +0000

    version bump for rhel fix

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ maintainer_email 'fraser.scott@burberry.com
 license          'MIT'
 description      'Installs/Configures logstash_forwarder'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'sensu_spec', '~> 0.2'
