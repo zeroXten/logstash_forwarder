16685c9f49b61a8e44e1d2f624b0eaa55f0e93e3
Fraser Scott <fraser.scott@burberry.com>
2014-02-18 11:14:42 +0000

    typo in proc check

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ maintainer_email 'fraser.scott@burberry.com
 license          'MIT'
 description      'Installs/Configures logstash_forwarder'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 depends 'sensu_spec', '~> 0.2'
