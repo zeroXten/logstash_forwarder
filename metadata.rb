b5c6ec27f519e3df02fab9f679791819f71bb5e4
Fraser Scott <fraser.scott@burberry.com>
2014-03-17 11:44:52 +0000

    cleaning up

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'logstash_forwarder'
 maintainer       'zeroXten'
 maintainer_email 'fraser.scott@gmail.com'
-'
+
 license          'MIT'
 description      'Installs/Configures logstash_forwarder'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
