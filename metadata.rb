bec13cd6a1c828b0148d89e9614f10befc07444f
Fraser Scott <fraser.scott@burberry.com>
2014-03-18 13:41:53 +0000

    rpm version for centos 5

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ maintainer_email 'fraser.scott@gmail.com'
 license          'MIT'
 description      'Installs/Configures logstash_forwarder'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.4'
+version          '1.0.5'
 
 depends 'sensu_spec', '~> 0.2'
