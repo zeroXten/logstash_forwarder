1637265c70304b02921f2130faa6f9d743d9b4f9
Fraser Scott <fraser.scott@gmail.com>
2014-04-02 13:35:21 +0100

    fields fix

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,4 +3,4 @@ default['logstash_forwarder']['config_file'] = '/etc/logstash-forwarder'
 default['logstash_forwarder']['user'] = 'root'
 default['logstash_forwarder']['group'] = 'root'
 default['logstash_forwarder']['config']['network']['servers'] = [ 'localhost:5043' ]
-default['logstash_forwarder']['config']['files']['default'] = { 'paths' => { '/var/log/messages' => true, '/var/log/*.log' => true, '/var/log/secure' => true }, 'type' => 'syslog' }
+default['logstash_forwarder']['config']['files']['default'] = { 'paths' => { '/var/log/messages' => true, '/var/log/*.log' => true, '/var/log/secure' => true }, 'fields' => { 'type' => 'syslog' } }
