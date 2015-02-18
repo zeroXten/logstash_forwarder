4adf615b1747875fa88f4e1f6ace7dd9f593fdc8
Fraser Scott <fraser.scott@gmail.com>
2015-02-18 17:06:58 +0000

    fixes from community

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'fraser.scott@gmail.com'
 license          'MIT'
 description      'Installs/Configures logstash_forwarder'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.5.0'
+version          '1.6.0'
 
 depends 'sensu_spec', '~> 0.10'
 depends 'apt', '>= 2.6'
