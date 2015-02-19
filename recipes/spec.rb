0e0a34f5a5f7caf1761abef2fe2a6ea211936cd0
svenbaum <svenbaum@gmail.com>
2015-02-19 10:17:29 +0000

    Update spec.rb
    removed more sensu_spec

--- recipes/spec.rb
+++ recipes/spec.rb
@@ -1,14 +1,1 @@
 
-describe 'logstash-forwarder' do
-  describe 'command' do
-    it 'must have executable file /opt/logstash-forwarder/bin/logstash-forwarder'
-  end
-
-  describe 'config' do
-    it "must have readable file #{node['logstash_forwarder']['config_file']}"
-  end
-
-  describe 'process' do
-    it 'must have 1 logstash-forwar process'
-  end
-end
